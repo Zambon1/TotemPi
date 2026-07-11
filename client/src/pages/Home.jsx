b670219ad5473b146c2ac604eb8adfe9d20dd29d
Zambon1 <mr360spectrum@gmail.com>
2026-07-10 18:19:40 -0600

    Using BrowserRouter

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+
+function Home() {
+
+    return (
+        <>
+        <div className="home-container">
+            <h1>Welcome to the Home Page</h1>
+        </div>
+        </>
+    );
+}
